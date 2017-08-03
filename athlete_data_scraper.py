9f0444d8246db4f43d14a8ba7894f3a6613cbdee
Charlotte Bermingham <cbermingham@hotmail.co.uk>
2017-08-03 14:11:41 +0100

    Made test data file

--- athlete_data_scraper.py
+++ athlete_data_scraper.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+import unicodedata
 
 #Get the website data
 r = requests.get('http://www.thepowerof10.info/athletes/profile.aspx?athleteid=6313')
@@ -10,6 +11,27 @@ test_data = r.text
 # Parse the data
 soup = BeautifulSoup(test_data, 'html.parser')
 
-#print soup.prettify()
+# Make it more readable
+pretty_data = soup.prettify()
 
-print soup.title
+# Save the pretty html to a file (have to change it from unicode to text)
+F = open('test_data.txt','w+') 
+F.write(pretty_data.encode('utf-8'))
+F.close()
+
+#table = soup.table
+#print table.string
+
+#print soup.find_all('a')
+
+#table = soup.find_all("PB")
+#print table
+
+#table = soup.find('table', {'class': "athleteprofilesubheader"})
+#print table.string
+
+table = soup.find(lambda tag: tag.name=='td' and tag.has_attr('id') and tag['id']=="cphBody_divBestPerformances") 
+
+print table
+#rows = table.findAll(lambda tag: tag.name=='tr')
+
