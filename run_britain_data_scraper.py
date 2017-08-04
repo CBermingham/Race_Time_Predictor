7f50cc447ac61425342b34c628f7118e83160566
Charlotte Bermingham <cbermingham@hotmail.co.uk>
2017-08-04 19:37:02 +0100

    scraper working, write data to file

--- run_britain_data_scraper.py
+++ run_britain_data_scraper.py
@@ -1,37 +1,109 @@
 import requests
 from bs4 import BeautifulSoup
+import csv
+import time
 
-#Get the website data
-r = requests.get('http://www.runbritainrankings.com/runners/profile.aspx?athleteid=17296')
+# -------------------------------------------
+#Get the urls of the individual athlete pages from the main page for an event
+# r = requests.get('http://www.runbritainrankings.com/rankings/rankinglist.aspx?event=5K&agegroup=V35&sex=M&year=2016')
 
-#Call the html data from the website a name
-test_data = r.text
+# athlete_data = r.text
 
-# Parse the data
-soup = BeautifulSoup(test_data, 'html.parser')
+# soup = BeautifulSoup(athlete_data, 'html.parser')
 
-# Make it more readable
-pretty_data = soup.prettify()
+# # Make the data pretty and save it so you can view it if needed
+# pretty_data = soup.prettify()
 
-# Save the pretty html to a file (have to change it from unicode to text)
-F = open('test_data_runbritain.txt','w+') 
-F.write(pretty_data.encode('utf-8'))
-F.close()
+# F = open('runbritain_athlete_data.txt','w+') 
+# F.write(pretty_data.encode('utf-8'))
+# F.close()
 
-div = soup.find("table",{"id":"cphBody_eventrankings_gvEventRankings"})
-tbody = div.find("tbody")
+# --------------------------------------------
 
-for row in tbody.find_all("tr"):
-	if row.find("td").next_sibling.text == '2016':
-		print row.find("td").text
-		print row.find("td").next_sibling.next_sibling.text
-#	if row.find("td").text == "2017":
-#		print row.find("td").text
+f = open('runbritain_athlete_data.txt', 'rU')
+lines=f.readlines()
+f.close()
 
-#row = div.find("tbody")
-#print row.find("td").text
-#print row.find("td").next_sibling.text
-#print row.find("td").next_sibling.next_sibling.text
+links = []
+ids = []
+for l in lines:
+	if "/runners/profile.aspx?athleteid=" in l:
+		links.append(l[l.find("/runners/"):l.find("target=")-3])
+		ids.append(l[l.find("athleteid=")+10:l.find("target=")-3])
 
-#table = div.next_sibling.find("table")
-#print table
+# -------------------------------------------
+
+possible_events = ['5K', '10K', 'HM', 'Mar']
+count = 0
+
+writer = csv.writer(open("runbritain_data_2016_men_5K_V35.csv", "wb"))
+writer.writerow(['athleteid'] + possible_events)
+
+for link in links:
+
+	#Get the website data
+	r = requests.get('http://www.runbritainrankings.com' + link)
+
+	#Call the html data from the website a name
+	test_data = r.text
+
+	# Parse the data
+	soup = BeautifulSoup(test_data, 'html.parser')
+
+	# Make it more readable
+	pretty_data = soup.prettify()
+
+	# Save the pretty html to a file (have to change it from unicode to text)
+	F = open('test_data_runbritain.txt','w+') 
+	F.write(pretty_data.encode('utf-8'))
+	F.close()
+
+
+
+	div = soup.find("table",{"id":"cphBody_eventrankings_gvEventRankings"})
+	tbody = div.find("tbody")
+
+	times = []
+	events = []
+	for row in tbody.find_all("tr"):
+		if row.find("td").next_sibling.text == '2016':
+			events.append(row.find("td").text)
+			times.append(row.find("td").next_sibling.next_sibling.text)
+
+	results = []
+	for i in range(0, len(possible_events)):
+		if possible_events[i] in events:
+	 		results.append(times[events.index(possible_events[i])].encode('utf-8'))
+	 	else:
+			results.append(None)
+
+	results = [ids[count]] + results
+
+	# # Save data as a csv file
+	writer = csv.writer(open("runbritain_data_2016_men_5K_V35.csv", "a"))
+	writer.writerow(results)
+
+	count += 1
+	if count % 15 == 0:
+		time.sleep(10)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
