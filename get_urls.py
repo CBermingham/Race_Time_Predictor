b9182424afe473d10b009e8d0b343f3300092b11
Charlotte Bermingham <cbermingham@hotmail.co.uk>
2017-08-04 09:58:08 +0100

    urls found

--- get_urls.py
+++ get_urls.py
@@ -17,34 +17,10 @@ tr = soup.find('tr', {"class":"rankinglistheadings"})
 
 links = []
 rows = tr.parent.find_all('tr')
-for i in rows:
-	links.append(i.find('a'))
+for i in rows[1:]:
+	if i.find('a') is not None:
+		links.append(i.find('a')['href'])
 
-# del links[0]
-# links2 = []
-# for i in range(0, len(links)):
-# 	if links[i] != 0:
-# 		links2.append(links[i])
+for i in links:
+	print i
 
-# for i in links2:
-# 	print i
-
-
-# if tr is not None:
-# 	for i in tr.siblings:
-# 		print i.find('a')
-# print tr.next_sibling.find('a')
-# print tr.next_sibling.next_sibling.find('a')
-
-#table = soup.find("table")
-# rows = table.find_all('tr')
-# for i in rows:
-# 	print i.find('td')
-
-
-# th = table.find('tr', {"class":"bestperformancesheader"}).next_sibling
-
-# for link in soup.find_all('a'):
-# 	new = link.encode('utf-8')
-# 	if "athleteid" in new:
-# 		print link
