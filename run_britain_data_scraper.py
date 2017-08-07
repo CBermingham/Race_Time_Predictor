44ced4d37ce577fce1edd0f8c2633a4114a26c61
Charlotte Bermingham <cbermingham@hotmail.co.uk>
2017-08-07 17:44:03 +0100

    correct age group - use from main page not athlete page

--- run_britain_data_scraper.py
+++ run_britain_data_scraper.py
@@ -4,42 +4,50 @@ import csv
 import time
 
 # -------------------------------------------
+
+distance = '5K'
+gender = 'M'
+year = '2016'
+
 #Get the urls of the individual athlete pages from the main page for an event
-# r = requests.get('http://www.runbritainrankings.com/rankings/rankinglist.aspx?event=5K&agegroup=V35&sex=M&year=2016')
+r = requests.get('http://www.runbritainrankings.com/rankings/rankinglist.aspx?event=' + distance + '&agegroup=ALL&sex=' + gender + '&year=' + year)
 
-# athlete_data = r.text
+athlete_data = r.text
 
-# soup = BeautifulSoup(athlete_data, 'html.parser')
+soup = BeautifulSoup(athlete_data, 'html.parser')
 
-# # Make the data pretty and save it so you can view it if needed
-# pretty_data = soup.prettify()
+# Make the data pretty and save it so you can view it if needed
+pretty_data = soup.prettify()
 
-# F = open('runbritain_athlete_data.txt','w+') 
-# F.write(pretty_data.encode('utf-8'))
-# F.close()
+F = open('runbritain_athlete_data_' + year + '_' + gender + '_' + distance + '.txt','w+') 
+F.write(pretty_data.encode('utf-8'))
+F.close()
 
 # --------------------------------------------
 
-f = open('runbritain_athlete_data.txt', 'rU')
+f = open('runbritain_athlete_data_' + year + '_' + gender + '_' + distance + '.txt', 'rU')
 lines=f.readlines()
 f.close()
 
 links = []
 ids = []
+age_group = []
 for l in lines:
 	if "/runners/profile.aspx?athleteid=" in l:
 		links.append(l[l.find("/runners/"):l.find("target=")-3])
 		ids.append(l[l.find("athleteid=")+10:l.find("target=")-3])
-
-# -------------------------------------------
+		if l[l.find("</a>', '")+8:l.find("</a>', '")+11] == "', ":
+			age_group.append("SEN")
+		else:
+			age_group.append(l[l.find("</a>', '")+8:l.find("</a>', '")+11])
 
 possible_events = ['5K', '10K', 'HM', 'Mar']
 count = 0
 
-writer = csv.writer(open("runbritain_data_2016_men_5K_V35.csv", "wb"))
-writer.writerow(['athleteid'] + possible_events)
+writer = csv.writer(open('runbritain_data_' + year + '_' + gender + '_' + distance + 'b.csv', "wb"))
+writer.writerow(['athleteid'] + ['age_group'] + possible_events)
 
-for link in links:
+for link in links[0:10]:
 
 	#Get the website data
 	r = requests.get('http://www.runbritainrankings.com' + link)
@@ -51,41 +59,40 @@ for link in links:
 	soup = BeautifulSoup(test_data, 'html.parser')
 
 	# Make it more readable
-	pretty_data = soup.prettify()
+#	pretty_data = soup.prettify()
 
 	# Save the pretty html to a file (have to change it from unicode to text)
-	F = open('test_data_runbritain.txt','w+') 
-	F.write(pretty_data.encode('utf-8'))
-	F.close()
-
-
+	# F = open('test_data_runbritain.txt','w+') 
+	# F.write(pretty_data.encode('utf-8'))
+	# F.close()
 
 	div = soup.find("table",{"id":"cphBody_eventrankings_gvEventRankings"})
-	tbody = div.find("tbody")
+	if div != None:
+		tbody = div.find("tbody")
 
-	times = []
-	events = []
-	for row in tbody.find_all("tr"):
-		if row.find("td").next_sibling.text == '2016':
-			events.append(row.find("td").text)
-			times.append(row.find("td").next_sibling.next_sibling.text)
+		times = []
+		events = []
+		for row in tbody.find_all("tr"):
+			if row.find("td").next_sibling.text == '2016':
+				events.append(row.find("td").text)
+				times.append(row.find("td").next_sibling.next_sibling.text)
 
-	results = []
-	for i in range(0, len(possible_events)):
-		if possible_events[i] in events:
-	 		results.append(times[events.index(possible_events[i])].encode('utf-8'))
-	 	else:
-			results.append(None)
+		results = []
+		for i in range(0, len(possible_events)):
+			if possible_events[i] in events:
+		 		results.append(times[events.index(possible_events[i])].encode('utf-8'))
+		 	else:
+				results.append(None)
 
-	results = [ids[count]] + results
+		results = [ids[count]] + [age_group[count]] + results
 
-	# # Save data as a csv file
-	writer = csv.writer(open("runbritain_data_2016_men_5K_V35.csv", "a"))
-	writer.writerow(results)
+		# # Save data as a csv file
+		writer = csv.writer(open('runbritain_data_' + year + '_' + gender + '_' + distance + 'b.csv', "a"))
+		writer.writerow(results)
 
-	count += 1
-	if count % 15 == 0:
-		time.sleep(10)
+		count += 1
+		if count % 15 == 0:
+			time.sleep(10)
 
 
 
